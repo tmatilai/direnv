17785e3dcccd2adce25316b19d2656c13feec30c
Teemu Matilainen <teemu.matilainen@iki.fi>
2022-01-14 19:42:59 +0200

    stdlib: Use "normal" color in log_status()
    Reset color for normal status messages.

--- stdlib.sh
+++ stdlib.sh
@@ -135,8 +135,10 @@ direnv_layout_dir() {
 log_status() {
   if [[ -n $DIRENV_LOG_FORMAT ]]; then
     local msg=$*
+    local color_normal
+    color_normal=$(tput sgr0)
     # shellcheck disable=SC2059,SC1117
-    printf "${DIRENV_LOG_FORMAT}\n" "$msg" >&2
+    printf "${color_normal}${DIRENV_LOG_FORMAT}\n" "$msg" >&2
   fi
 }
 
